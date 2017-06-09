aeda4e5777f50607038a1bf994a11fa8e4e793d2
Jehian Norman Saviero <jehiannormansaviero@gmail.com>
2017-06-09 20:38:31 +0700

    Test web

---
+++ test/js/openrecruitment.js
@@ -0,0 +1,213 @@
+// JavaScript Document
+$(function(){
+  var division = [
+    'Relasi Tahap Persiapan Bersama',
+    'Relasi S1 ITB Ganesha',
+    'Relasi ITB Jatinangor',
+    'Relasi ITB Cirebon',
+    'Relasi Pascasarjana dan Hubungan Masyarakat',
+    'Publikasi Dalam Jaringan',
+    'Publikasi Luar Jaringan',
+    'Kreasi Visual',
+    'Kajian Strategis',
+    'Analisis Data dan Aspirasi'
+  ];
+
+  var divisionItemTemplate=
+    '<div class="col s12 division-item">'
+    + '<div class="panel panel-default">'
+    + '<div class="panel-heading"><span></span></div></div></div>';
+
+  var divisionItemPlaceholder=
+    '<div class="col s12 division-item division-item-placeholder">'
+    + '<div class="panel panel-default">'
+    + '<div class="panel-heading"><span></span></div></div></div>';
+
+  $('.datepicker').pickadate({
+  	format:'dd/mm/yyyy'
+  });
+
+  var isShuffled=false;
+
+  function initializePool() {
+        var pool = $('.division-pool');
+        pool.html('');
+        for (i = 0; i < division.length; ++i) {
+            var newDivisionItem = $($.parseHTML(divisionItemTemplate));
+            newDivisionItem.data('division', division[i]);
+            pool.append(newDivisionItem);
+        }
+        // append clear to make the well works
+        pool.append('<div class="clear"></div>');
+    }
+
+  initializePool();
+
+   function shuffle(){
+        for(i = 1;i <= 100; ++i){
+            var x = Math.floor(Math.random() * i) % division.length;
+            var y = Math.floor(Math.random() * (i + 1)) % division.length;
+            var temp = division[x];
+            division[x] = division[y];
+            division[y] = temp;
+        }
+        initializePool();
+        reorderDivisionItem();
+        isShuffled = true;
+    }
+
+    // initially, shuffle the options
+    shuffle();
+
+ function reorderDivisionItem(ui) {
+        // reorder the numbering
+        $('.division-item:not(.ui-sortable-helper)').each(function(i, e) {
+            // convert into jQuery object
+            e = $(e);
+            if (e.hasClass('division-item-placeholder')) {
+                e = ui.helper;
+            }
+            var name = e.data('division');
+            e.find('span').html('' + (i + 1) + '. ' + name);
+
+            // bold the top three, replace them in text
+            e.removeClass('division-item-top');
+            if (i < 3) {
+                e.addClass('division-item-top');
+                $('.division-' + (i + 1)).html(name);
+            }
+        });
+    }
+    reorderDivisionItem();
+
+    $('.division-pool').sortable({
+        cursor: 'move',
+        items: '.division-item',
+        opacity: 0.8,
+        update: function (event, ui) {
+            reorderDivisionItem();
+            isShuffled = false;
+        },
+        sort: function (event, ui) {
+            reorderDivisionItem(ui);
+        },
+        placeholder: {
+            element: function(clone, ui) {
+                return $(divisionItemPlaceholder);
+            },
+            update: function() {}
+        },
+        revert: 300
+    });
+
+  particlesJS("particles-js",{
+    "particles": {
+      "number": {
+        "value": 100,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#1e00ff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0,
+        "random": false,
+        "anim": {
+          "enable": false,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+          "enable": false,
+          "speed": 40,
+          "size_min": 0.1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 125,
+        "color": "#004dbb",
+        "opacity": 0.2,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "top-right",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": true,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "window",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 180,
+          "line_linked": {
+            "opacity": 0.5
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+});
